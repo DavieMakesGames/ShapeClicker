c297f82f0148c079e2ea3cc640f77a2c2a1f57c1
David Perez <davidperezdev@gmail.com>
2021-01-10 16:48:47 -0500

    Menu & Shape-Shifting
    Includes support for changing shape and color.

---
+++ Assets/Scripts/ImageHitTestIgnoreZeroAlpha.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class ImageHitTestIgnoreZeroAlpha : MonoBehaviour
+{
+    public Image Button;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button.alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
---
+++ Assets/Scripts/ShapeShifterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShapeShifterScript : MonoBehaviour
+{
+    private float _lastClickTime;
+
+    public const float DOUBLE_CLICK_TIME = .3f;
+    public Image Image;
+    public TMP_Dropdown Dropdown;
+
+    void Start()
+    {
+        //Add listener for when the value of the Dropdown changes, to take action
+        Dropdown.onValueChanged.AddListener(delegate {
+            ChangeShape(Dropdown.options[Dropdown.value].image);
+        });
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShapeClick()
+    {
+        float clickDelta = Time.time - _lastClickTime;
+        if (clickDelta <= DOUBLE_CLICK_TIME) ChangeColor();
+        _lastClickTime = Time.time;
+    }
+
+    public void ChangeColor()
+    {
+        Image.color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    public void ChangeShape(Sprite img)
+    {
+        Image.sprite = img;
+    }
+}
